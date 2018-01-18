eb89f9375aa59ff6fe1673216fa2453ed6d5bff4
kawin Liaowongphuthorn <liaokangtai@gmail.com>
2018-01-18 08:58:20 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
   for i in 0..5
-    print "#{colors[i][0].upcase}: #{colors[i], }"
+    print "#{colors[i][0].upcase}: #{colors[i]}, "
   end
   print "#{colors[6][0].upcase}: #{colors[6]}"
 end
